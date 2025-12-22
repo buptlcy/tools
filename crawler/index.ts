3b05c9d09f568d0945b310a278674fd3b86dc953
lcy <lcyinvincibleby@163.com>
2025-12-22 22:51:31 +0800

    init: add dot dev

--- crawler/index.ts
+++ crawler/index.ts
@@ -1,6 +1,2 @@
-import fs from 'node:fs'
-
 console.log(1)
 console.log('1')
-
-const v: any = 1
--- eslint.config.js
+++ eslint.config.js
@@ -13,4 +13,13 @@ export default antfu({
         'semi': ['error', 'never'], // 强制分号
         'max-len': ['error', { code: 120 }], // 行宽限制
     },
+    ignores: [
+        'dist/**/*', // 排除编译产物目录
+        'node_modules/**/*', // 排除依赖目录（默认已排除，可省略）
+        'scripts/build.js', // 排除单个文件
+        '*.config.js', // 排除所有根目录的.config.js文件（如vite.config.js）
+        'src/test/**/*.ts', // 排除src/test下的所有TS文件
+        '.env',
+        '.env.prod',
+    ],
 })
